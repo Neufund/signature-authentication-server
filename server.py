6398412d3537833693ce55a19de7e332ab844510
Remco Bloemen <remco-git@xn--2-umb.com>
2017-06-01 23:43:18 +0100

    Turn more errors into JSON

--- server.py
+++ server.py
@@ -20,15 +20,21 @@ def login():
     data = request.get_json()["signature"]
     return data
 
+@app.errorhandler(400)
+def bad_request(ex):
+    return jsonify({"code": 400, "message": ex.description}), 400
 
 @app.errorhandler(403)
 def forbidden(ex):
     return jsonify({"code": 403, "message": ex.description}), 403
 
-
 @app.errorhandler(404)
 def not_found(ex):
     return jsonify({"code": 404, "message": ex.description}), 404
+
+@app.errorhandler(500)
+def internal_error(ex):
+    return jsonify({"code": 500, "message": ex.description}), 500
 
 
 if __name__ == '__main__':

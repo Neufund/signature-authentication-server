b0f7b8d0ce888fd62d3702e1ceadb503b6eaab65
Remco Bloemen <remco-git@xn--2-umb.com>
2017-06-01 23:44:51 +0100

    Load private keys for JWT

--- config.py
+++ config.py
@@ -2,16 +2,16 @@ from datetime import timedelta
 
 AUDIENCE = None
 LOGIN_ALGORITHM = "ES512"
-PUB_KEY_PATH = "ec512.pub.pem"
+PRV_KEY_PATH = "/srv/ec512.prv.pem"
 ISSUER = "Neufund"
-PUBLIC_ECDSA_KEY = None
+PRIVATE_ECDSA_KEY = None
 LOGIN_TOKEN_LIFE_TIME = timedelta(minutes=30)
 
 
 def read_keys():
-    global PUBLIC_ECDSA_KEY
-    with open(PUB_KEY_PATH, "r") as publicKey:
-        PUBLIC_ECDSA_KEY = publicKey.read()
+    global PRIVATE_ECDSA_KEY
+    with open(PRV_KEY_PATH, "r") as privateKeyFile:
+        PRIVATE_ECDSA_KEY = privateKeyFile.read()
 
 
 read_keys()

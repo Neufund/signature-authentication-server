ca5d9e4f0881abdd60dc1fd711df11270a8f8926
Remco Bloemen <remco-git@xn--2-umb.com>
2017-06-03 14:12:55 +0100

    Add negative test cases for all bits

--- test.py
+++ test.py
@@ -1,19 +1,40 @@
 import unittest
 from signature import *
 
+def bit_flip(bytes, pos):
+    return (int.from_bytes(bytes, 'big') ^ 2**pos).to_bytes(len(bytes), 'big')
+
 class VerifyTest(unittest.TestCase):
 
+    address = '0x29c76e6ad8f28bb1004902578fb108c507be341b'
+    message = 'Hello, world!'.encode('utf8')
+    hash = bytes.fromhex('b453bd4e271eed985cbab8231da609c4ce0a9cf1f763b6c1594e76315510e0f1')
+    signature = bytes.fromhex('90a938f7457df6e8f741264c32697fc52f9a8f867c52dd70713d9d2d472f2e415d9c94148991bbe1f4a1818d1dff09165782749c877f5cf1eff4ef126e55714d1c')
+
+
     def test_hash(self):
-        message = 'Hello, world!'
-        ehash = bytes.fromhex('b453bd4e271eed985cbab8231da609c4ce0a9cf1f763b6c1594e76315510e0f1')
-        hash = message_hash(message)
-        self.assertEqual(hash, ehash)
+        hash = message_hash(self.message)
+        self.assertEqual(self.hash, hash)
 
     def test_verify(self):
-        address = '0x29c76e6ad8f28bb1004902578fb108c507be341b'
-        message = 'Hello, world!'
-        signature = bytes.fromhex('90a938f7457df6e8f741264c32697fc52f9a8f867c52dd70713d9d2d472f2e415d9c94148991bbe1f4a1818d1dff09165782749c877f5cf1eff4ef126e55714d1c')
-        self.assertEqual(verify(address, message, signature), True)
+        self.assertEqual(verify(self.address, self.message, self.signature), True)
+
+    def test_false_address(self):
+        address_bytes = bytes.fromhex(self.address[2:])
+        for i in range(len(address_bytes) * 8):
+            address = bit_flip(address_bytes, i)
+            address_enc = '0x' + address.hex()
+            self.assertEqual(verify(address_enc, self.message, self.signature), False)
+
+    def test_false_message(self):
+        for i in range(len(self.message) * 8):
+            message = bit_flip(self.message, i)
+            self.assertEqual(verify(self.address, message, self.signature), False)
+
+    def test_false_signature(self):
+        for i in range(len(self.signature) * 8):
+            signature = bit_flip(self.signature, i)
+            self.assertEqual(verify(self.address, self.message, signature), False)
 
 if __name__ == '__main__':
     unittest.main()
